dc04d582670da3634caa9f9bac63036e9c9445cd
Weixin07 <77734173+Weixin07@users.noreply.github.com>
2022-10-06 17:41:44 +0800

    Lab 9 Task 3 Start

---
+++ Lab9/Task3StackWithList/Task3StackWithList/Code.cpp
@@ -0,0 +1,21 @@
+//exercise 1: parenthesis balancing exercise using linked list implementation
+
+#include <iostream>
+using namespace std;
+
+class stack //using double linked list based stack
+{
+	//push, pop, isEmpty
+};
+
+int main()
+{
+	stack s1;
+	string equation = "";
+
+	cout << "Enter an equation: " << endl;
+	cin >> equation;
+
+	//parenthese checking after get the equation from the user
+	return 0;
+}
