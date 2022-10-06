3a388f0ccb4198f4ffcfc2e34f24766d2a5aa055
Weixin07 <77734173+Weixin07@users.noreply.github.com>
2022-10-06 17:39:23 +0800

    Task 2 done Lab 9

--- Lab9/Stack/Stack/Ex2.cpp
+++ Lab9/Stack/Stack/Ex2.cpp
@@ -12,10 +12,13 @@ class stack
         stacknode* previous;
     } *top, *bottom; //top = head, bottom = tail
 
+    int stackID;
+
 public:
     //Step 2: We need to ensure top and bottom should start from NULL
-    stack() { //constructor to define default value
+    stack(int ID) { //constructor to define default value
         top = bottom = NULL; // whenever create a new stack object it will start from empty
+        stackID = ID;
     }
 
     //Step 3: We meed to set the push function - insert to the front
@@ -37,17 +40,46 @@ public:
         }
     }
 
+    //Step 4: Delete from the front of list
     void pop() {
+        //4.1 Check if the stack is empty or not
+        if (top == NULL) {
+            cout << "Stack is underflow! Unable to delete!" << endl;
+            return;
+        }
 
+        //4.2 declare a new current pointer
+        stacknode* current = top;
+        top = top->next;
+
+        //4.3 Check whether top is equal to NULL
+        if (top != NULL)
+            top->previous = NULL;
+        else
+            bottom = NULL;
+
+        //4.4 delete the current item
+        cout << "Pop: Item " << current->studentID << endl;
+        delete current;
     }
 
     void displayitemsInStack() {
+        //delete from top to bottom
+        stacknode* current = top;
 
+        cout << "Stack: " << stackID << endl;
+        while (current != NULL) {
+            cout << current->studentID << ", ";
+            current=current->next;
+        }
+        cout << endl;
     }
-
+    
+    //To view the latest top value without removing it. only view it
     void peek()
     {
-
+        stacknode* current = top;
+        cout << "Top value: " << current->studentID << endl;
     }
 };
 
@@ -55,48 +87,63 @@ public:
 int main()
 {
     //create object stack and modify the below code:
+    stack stack1(1);
+    stack stack2(2);
 
-    /*push("TP01");
-    displayitemsInStack();
+    stack1.push("TP01");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP02");
-    displayitemsInStack();
+    stack1.push("TP02");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP03");
-    displayitemsInStack();
+    stack1.push("TP03");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    pop();
-    displayitemsInStack();
+    stack1.pop();
+    stack1.displayitemsInStack();
     cout << endl;
 
-    peek();
+    stack1.peek();
     cout << endl;
 
-    pop();
-    displayitemsInStack();
+    stack1.pop();
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP04");
-    displayitemsInStack();
+    stack1.push("TP04");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP05");
-    displayitemsInStack();
+    stack1.push("TP05");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP03");
-    displayitemsInStack();
+    stack1.push("TP03");
+    stack1.displayitemsInStack();
     cout << endl;
 
-    pop();
-    displayitemsInStack();
+    stack1.pop();
+    stack1.displayitemsInStack();
     cout << endl;
 
-    push("TP02");
-    displayitemsInStack();
-    cout << endl;*/
+    stack1.push("TP02");
+    stack1.displayitemsInStack();
+    cout << endl;
+
+    stack2.push("TP11");
+    stack2.displayitemsInStack();
+    cout << endl;
+
+    stack2.push("TP12");
+    stack2.displayitemsInStack();
+    cout << endl;
+
+    stack2.pop();
+    stack2.displayitemsInStack();
+    cout << endl;
+
     return 0;
 }
